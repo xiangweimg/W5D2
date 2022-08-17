2004746d550df716b5b539eb2f55cec3c663f7b2
Xiangwei Liu <xiangweimg@gmail.com>
2022-08-17 11:03:53 -0400

    07 wokringon

--- w5d2_classwork/skeleton/lib/07_joins_exercises.rb
+++ w5d2_classwork/skeleton/lib/07_joins_exercises.rb
@@ -40,6 +40,14 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT
+  title
+  FROM 
+  movies
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE
+  actors.name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +56,25 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+  SELECT
+  title
+  FROM 
+  movies
+  JOIN castings ON movies.id = castings.movie_id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE
+  actors.name = 'Harrison Ford' AND castings.ord != 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+  SELECT
+  title, actors.name
+  FROM
+  movies
+  JOIN 
   SQL
 end
 
